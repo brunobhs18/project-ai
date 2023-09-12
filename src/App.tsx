31eb4395ca457b83f36daea28bd78385697a299f
Bruno Henrique <brunobhs18@gmail.com>
2023-09-12 14:07:30 -0300

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,131 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { Github, FileVideo, Upload, Wand2 } from 'lucide-react'
+import { Button } from "./components/ui/button";
+import { Separator } from './components/ui/separator';
+import { Textarea } from './components/ui/textarea';
+import { Label } from './components/ui/label';
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from './components/ui/select';
+import { Slider } from './components/ui/slider';
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export function App() {  
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className='min-h-screen flex flex-col'>
+      <div className="px-6 py-3 flex items-center justify-between border-b">
+        <h1 className="text-xl font-bold">project.ai</h1>
+
+        <div className="flex items-center gap-3">
+          <span 
+            className="text-sm text-muted-foreground">
+              Desenvolvido no NLW 
+          </span>
+
+          <Separator orientation='vertical' className='h-6'/>
+          <Button variant={'outline'}>
+            <Github className='w-4 h-4 mr-2'/>
+            Github
+          </Button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+
+      <main className='flex-1 p-6 flex gap-6'>
+          <aside className='w-80 space-y-6'>
+            <form className='space-y-6'>
+
+              <label 
+                className='border flex rounded-md aspect-video items-center justify-center cursor-pointer border-dashed text-sm flex-col gap-2 text-muted-foreground hover:bg-primary/5'
+                htmlFor="video">
+
+                <FileVideo />
+                Selecione o vídeo
+              </label>
+
+              <input type="file" id='video' accept='video/mp4' className='sr-only'/>
+
+              <Separator />
+
+              <div className='space-y-2'>
+                <Label htmlFor='transcription_prompt'>Prompt de transcrição</Label>
+                <Textarea 
+                  id='transcription_prompt' 
+                  className='h-20 leading-relaxed resize-none'
+                  placeholder='Inclua palavras-chave mencionadas no vídeo separadas por vírgula (,)'
+                />
+              </div>
+
+              <Button type='submit' className='w-full'>
+                Carregar vídeo
+                <Upload className='w-4 h-4 ml-2'/>
+              </Button>
+            </form>
+
+            <Separator />
+
+            <form className='space-y-6'>
+
+            <div className='space-y-2'>
+                <Label>Prompt</Label>
+                <Select>
+                  <SelectTrigger>
+                    <SelectValue placeholder="Selecione um prompt..." />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value='title'>Título do Youtube</SelectItem>
+                    <SelectItem value='description'>Descrição do Youtube</SelectItem>
+                  </SelectContent>
+                </Select>                       
+              </div>
+
+              <div className='space-y-2'>
+                <Label>Modelo</Label>
+                <Select disabled defaultValue='gpt3.5'>
+                  <SelectTrigger>
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectItem value='gpt3.5'>GPT 3.5-turbo 16k</SelectItem>
+                  </SelectContent>
+                </Select>
+                <span className='block text-xs text-muted-foreground italic'>Você poderá customizar essa opção em breve</span>         
+              </div>
+
+              <Separator />
+
+              <div className='space-y-4'>
+                <Label>Temperatura</Label>
+                <Slider 
+                  min={0}
+                  max={1}
+                  step={0.1}
+                />
+                <span className='block text-xs text-muted-foreground italic leading-relaxed'>Valores mais altos tendem a deixar o resultado mais criativo e com possíveis erros.</span>         
+              </div>
+
+              <Separator />
+
+              <Button type='submit' className='w-full'>
+                Executar
+                <Wand2 className='w-4 h-4 ml-2'/>
+              </Button>
+
+            </form>
+
+          </aside>
+
+          <div className='flex flex-col flex-1 gap-4'>
+            <div className='grid grid-rows-2 gap-4 flex-1'>
+              <Textarea 
+                  className='resize-none p-5 leading-relaxed'
+                  placeholder='Inclua o prompt para a IA...'
+              />
+              <Textarea 
+                  className='resize-none p-5 leading-relaxed'
+                  placeholder='Resultado gerado pela IA...' readOnly
+              />
+            </div>
+            <p className='text-sm text-muted-foreground'>Lembre-se: você pode utilizar a variável <code className='text-cyan-400'>{'{transcription}'}</code> no seu prompt para adicionar o conteúdo da transcrição do vídeo selecionado.</p>
+          </div>
+      </main>
+    </div>
   )
 }
 
-export default App
+
