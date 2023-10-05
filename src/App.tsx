f4889558241f1911c3457d399307c6c78c92d08e
Bruno Henrique <116025082+brunobhs18@users.noreply.github.com>
2023-10-05 09:02:22 -0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -24,7 +24,7 @@ export function App() {
     completion,
     isLoading,
   } = useCompletion({
-    api: 'http://localhost:3333/ai/complete',
+    api: 'https://api-ai-nlw.vercel.app/ai/complete',
     body: {
       videoId,
       temperature,      
@@ -42,7 +42,7 @@ export function App() {
         <div className="flex items-center gap-3">
           <span 
             className="text-sm text-muted-foreground">
-              Desenvolvido no NLW 
+              Desenvolvido em participação do evento NLW-IA
           </span>
 
           <Separator orientation='vertical' className='h-6'/>
